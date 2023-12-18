649bdb9b0b953eebc20570447d4bfc3865a1e4fe
CaptainFromPlane <arthur.curie@ipsa.fr>
2023-12-18 09:41:00 +0100

    using if

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main() {
-	printf("hello world\n");
-
+	int a = 0;
+	int b = 5;
+	printf("a = %d\n",a);
+	printf("b = %d\n",b);
+	printf("a + b = %d \n",a+b);
+	
+	if (a > b){
+		printf("a > b\n");
+	} else {
+		printf("a < b\n");
+	}
+		
 	return 0;
 }
